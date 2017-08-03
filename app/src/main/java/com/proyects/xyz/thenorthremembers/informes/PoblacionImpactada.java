6674b144d9e29de6432b4f5c569d96782c02aa8c
Chiting Ching <chiting23@outlook.com>
2017-08-02 21:35:25 -0500

    primer paaso

---
+++ app/src/main/java/com/proyects/xyz/thenorthremembers/informes/PoblacionImpactada.java
@@ -0,0 +1,15 @@
+package com.proyects.xyz.thenorthremembers.informes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.proyects.xyz.thenorthremembers.R;
+
+public class PoblacionImpactada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poblacion_impactada);
+    }
+}
